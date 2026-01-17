00437640fc9488dc4d662928f2deb2da05551182
liza-23 <jannatulfardus1243@gmail.com>
2026-01-18 00:30:03 +0600

    dashboard panel

---
+++ Admin/HTML/dashboard.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../CSS/adminDashboard.css">
+    <link rel="stylesheet" href="../../Shared/CSS/navbar.css">
+    
+    
+</head>
+<body>
+    
+    <?php include '../../Shared/HTML/navbar.php'; ?>
+
+    <div class="dashboard-container">
+        
+        <div class="dashboard-head-card">
+            <div class="title-section">
+                <h2>Event Management</h2>
+                <p>Manage your upcoming events effortlessly.</p>
+            </div>
+            <a href="addEvent.php" class="btn-add-new">
+                <span>+</span> Post New Event
+            </a>
+        </div>
+        
+
+    </div>
+
+</body>
+</html>
